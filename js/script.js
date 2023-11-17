d37578ee25ba9427f98ae007192bdafbcfe90925
AimanFadil <aimanbello2001@gmail.com>
2023-11-17 16:05:44 +0100

    push 2 di controllo

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ let color = prompt (`Qual e il tuo colore preferito?`);
 
 let benvenuto = `Benvenuto ${name} ${surname} il tuo colore preferito è ${color} 23`;
 
-document.getElementById(`subtitle`).innerHTML - benvenuto;
+document.getElementById(`subtitle`).innerHTML = benvenuto;
 console.log(benvenuto)
