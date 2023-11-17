f76e9eb0713569c510b74f9ba6856fa022f86630
AimanFadil <aimanbello2001@gmail.com>
2023-11-17 15:57:11 +0100

    corrections

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-let name = prompt ('Qual e il tuo nome?');
+let name = prompt (`Qual e il tuo nome?`);
 
-let surname = prompt ('Qual e il tuo cognome?');
+let surname = prompt (`Qual e il tuo cognome?`);
 
-let color = prompt ('Qual e il tuo colore preferito?');
+let color = prompt (`Qual e il tuo colore preferito?`);
 
-let benvenuto = 'Benvenuto ${name} ${surname} il tuo colore preferito è ${color} 23';
+let benvenuto = `Benvenuto ${name} ${surname} il tuo colore preferito è ${color} 23`;
 
-document.getElementById('subtitle').innerHTML - benvenuto;
+document.getElementById(`subtitle`).innerHTML - benvenuto;
