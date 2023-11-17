400b677393b3900fcdba483f96ab031206198fce
AimanFadil <aimanbello2001@gmail.com>
2023-11-17 15:42:09 +0100

    push di controllo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let name = prompt ('Qual e il tuo nome?');
+
+let surname = prompt ('Qual e il tuo cognome?');
+
+let color = prompt ('Qual e il tuo colore preferito?');
+
+let benvenuto = 'Benvenuto ${name} ${surname} il tuo colore preferito è ${color} 23';
+
+document.getElementById('subtitle').innerHTML - benvenuto;
