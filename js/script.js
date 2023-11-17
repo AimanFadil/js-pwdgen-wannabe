c216e5c4a5de9af35d8c5129ce5c8f51c9034f12
AimanFadil <aimanbello2001@gmail.com>
2023-11-17 16:14:38 +0100

    push 3 di controllo

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ let surname = prompt (`Qual e il tuo cognome?`);
 
 let color = prompt (`Qual e il tuo colore preferito?`);
 
-let benvenuto = `Benvenuto ${name} ${surname} il tuo colore preferito è ${color} 23`;
+let benvenuto = `${name}${surname}${color}23`;
 
 document.getElementById(`subtitle`).innerHTML = benvenuto;
 console.log(benvenuto)
+
